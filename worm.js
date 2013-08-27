c27a6d04cbba043fd9bc41a4fac8f4e827014b37
Jeremie Miller <jeremie@jabber.org>
2013-08-26 18:12:05 -0600

    first minimum working version, not stable yet

---
+++ worm.js
@@ -0,0 +1,54 @@
+var fs = require("fs");
+var tele = require("telehash");
+var argv = require("optimist")
+  .usage("Usage: type '$0' to receive (prints it's hashname) and 'cat foo.txt | $0 hashdestination' to send")
+  .default("id", process.env.HOME + "/.worm.json")
+  .default("seeds", "./seeds.json")
+  .boolean("v").describe("v", "verbose")
+  .argv;
+
+if(argv.v) tele.debug(console.log);
+var holehash = argv._[0];
+if(holehash && !tele.isHashname(holehash))
+{
+  console.error("invalid destination hashname (must be 64 hex characters):",holehash);
+  process.exit(1);
+}
+
+var worm;
+var hole;
+
+// load or generate our crypto id
+if(fs.existsSync(argv.id))
+{
+  init(require(argv.id));
+}else{
+  tele.genkey(function(err, key){
+    fs.writeFileSync(argv.id, JSON.stringify(key, null, 4));
+    init(key);
+  });
+}
+
+function init(key)
+{
+  worm = tele.hashname(key);
+
+  require(argv.seeds).forEach(worm.addSeed);
+
+  worm.online(function(err){
+    if(err) {
+      console.error(err);
+      process.exit(0);
+    }
+    if(holehash) {
+      hole = worm.wrap(worm.stream(holehash).send({type:"_wormhole"}));
+      process.stdin.pipe(hole);
+    } else console.error("This worm is:",worm.hashname);
+  });
+
+  worm.listen("_wormhole", function(chat, packet, callback){
+    if(callback) callback();
+    packet.stream.send({}); // ack
+    worm.wrap(packet.stream).pipe(process.stdout);
+  });
+}
